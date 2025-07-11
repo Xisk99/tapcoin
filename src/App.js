4ecb4878c7154053ce3abc4d4bfcb72430e724cf
Luis Rivera <luisriveradiaz1699@gmail.com>
2025-07-11 10:20:30 -0500

    dsada

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 console.log('Environment Variables:', process.env);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function App() {
   const [walletAddress, setWalletAddress] = useState(null);
